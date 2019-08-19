fec908d21f40fad9b5304edb0834ad2843b76f17
Connor Weideman <connor@cmfreelancing.co.za>
2019-08-19 08:17:25 +0200

    Update 1

--- index.php
+++ index.php
@@ -2,17 +2,29 @@
 
 <main>
     <section id="main1">
-        <div id="gradient-overlay"></div>
         <img src="http://localhost/test/wordpress/wp-content/uploads/2019/08/RichCon-logo.png" alt="logo">
+        <div id="gradient-overlay"></div>
     </section>
     <section id="main2">
-        <h1>Main2</h1>
+        <img src="http://localhost/test/wordpress/wp-content/uploads/2019/08/Henck-Richard-Pose1.jpg">
+        <div id="gradient-overlay"></div>
+        <div id="logo">
+            <img src="http://localhost/test/wordpress/wp-content/uploads/2019/08/RichCon-logo.png" alt="logo">        
+        </div>
     </section>
     <section id="main3">
-        <h1>Main3</h1>
+        <img src="http://localhost/test/wordpress/wp-content/uploads/2019/08/Henck-Richard-Pose1.jpg">
+        <div id="gradient-overlay"></div>
+        <div id="header">
+            <div class="wrap">
+                <img src="http://localhost/test/wordpress/wp-content/uploads/2019/08/RichCon-logo.png" alt="logo">
+                <h1>PRODUCERS, ACTORS, DIRECTORS</h1>     
+            </div>
+        </div>
     </section>
     <section id="main4">
-        <h1>Main4</h1>
+        <img src="http://localhost/test/wordpress/wp-content/uploads/2019/08/RichCon-logo.png" alt="logo">
+        <div id="gradient-overlay"></div>
     </section>
     <input type="range" max="3" min="0" value="0">
 </main>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,15 +3,12 @@ function toggleNavStyle() {
     const topNav = document.querySelector("header#top");
     const sideNav = document.querySelector("header#side");
     const mainContent = document.querySelector("main");
-    const main1Overlay = document.querySelector("main #main1 #gradient-overlay");
     const mainToggle = document.querySelector("main input");
     const footer = document.querySelector("footer");
-
     if (navStyleToggle) {
         topNav.style.display = "none";
         sideNav.style.display = "block";
         mainContent.style.padding = `0 0 0 ${sideNav.clientWidth}px`;
-        main1Overlay.style.margin = "0";
         mainToggle.style.margin = `0 0 10px ${sideNav.clientWidth + 10}px`;
         footer.style.padding = `50px 0 50px ${sideNav.clientWidth}px`;
     }
@@ -19,11 +16,36 @@ function toggleNavStyle() {
         topNav.style.display = "block";
         sideNav.style.display = "none";
         mainContent.style.padding = `${topNav.clientHeight}px 0 0 0`;
-        main1Overlay.style.margin = `${topNav.clientHeight}px 0 0 0`;
         mainToggle.style.margin = `0 0 10px ${sideNav.clientWidth + 10}px`;
         footer.style.padding = "50px 0";
     }
+    toggleNavMains()
     navStyleToggle = !navStyleToggle;
+}
+
+function toggleNavMains() {
+    const vh = document.documentElement.clientHeight;
+    const topNav = document.querySelector("header#top");
+    const main1 = document.querySelector("main #main1");
+    const main1Overlay = document.querySelector("main #main1 #gradient-overlay");
+    const main2 = document.querySelector("main #main2");
+    const main3 = document.querySelector("main #main3");
+    if (navStyleToggle) {
+        main1.style.height = "100vh";
+        main1.style.lineHeight = "100vh";
+        main1.style.transition = "3s";
+        main1Overlay.style.margin = "0";
+        main2.style.height = "100vh";
+        main3.style.height = "100vh";
+    }
+    else {
+        main1.style.transition = "0s";
+        main1.style.height = `${vh - topNav.clientHeight}px`;
+        main1.style.lineHeight = `${vh - topNav.clientHeight}px`;
+        main1Overlay.style.margin = `${topNav.clientHeight}px 0 0 0`;
+        main2.style.height = `${vh - topNav.clientHeight}px`;
+        main3.style.height = `${vh - topNav.clientHeight}px`;
+    }
 }
 
 document.querySelector("#nav-position-toggler").addEventListener("click", toggleNavStyle);
@@ -93,15 +115,70 @@ function toggleMain() {
         }
         else mainContent[i].style.display = "none";
     }
+    main1();
+    main2();
+    main3();
+    main4();
 }
 mainToggle.addEventListener("change", toggleMain);
 
 function main1() {
-    setTimeout(function() {
-        const mainOverlay = document.querySelector("main #main1 #gradient-overlay");
-        const mainImg = document.querySelector("main #main1 img");
-        mainOverlay.style.height = "100%";
-        mainImg.style.width = "50%";
-    }, 1000);
+    const mainOverlay = document.querySelector("main #main1 #gradient-overlay");
+    const mainImg = document.querySelector("main #main1 img");
+    if (mainToggle.value == 0) {
+        setTimeout(function() {
+                mainOverlay.style.height = "100%";
+                mainImg.style.width = "50%";
+            }, 1000);
+    }
+    else {
+        mainOverlay.style.height = "0%";
+        mainImg.style.width = "0%";
+
+    }
 }
 document.addEventListener("DOMContentLoaded", main1);
+
+function main2() {
+    const mainOverlay = document.querySelector("main #main2 #gradient-overlay");
+    if (mainToggle.value == 1) {
+        setTimeout(function() {
+            mainOverlay.style.opacity = "0.6";
+        }, 500);
+    }
+    else {
+        mainOverlay.style.opacity = "1";
+    }
+}
+
+let main3Interval;
+function main3() {
+    const headerWrap = document.querySelector("main #main3 #header .wrap");
+    const backgroundImage = document.querySelector("main #main3 > img");
+    let headerToggle = true;
+    if (mainToggle.value == 2) {
+        setTimeout(()=>{backgroundImage.style.transform = "scale(1.1)"}, 250);
+        main3Interval = setInterval(function() {
+            if (headerToggle) {
+                headerWrap.style.top = "-100%";
+            }
+            else {
+                headerWrap.style.top = "0";
+            }
+            headerToggle = !headerToggle;
+        }, 3000);
+    }
+    else {
+        backgroundImage.style.transform = "scale(1)";
+        clearInterval(main3Interval);
+    }
+}
+
+function main4() {
+    const overaly = document.querySelector("main #main4 #gradient-overlay");
+    if (mainToggle.value == 3) {
+        setTimeout(function() {
+        overaly.style.opacity = "0";
+        },500);
+    }
+}
